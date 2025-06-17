e6d8c7d1e3b717988a685c288d9e05284bd2f11b
DanielSeton <danielseton@live.nl>
2025-06-17 17:53:08 +0200

    get request toegevoegd op overview pagina

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,14 +1,42 @@
 import "./Overview.css"
 import BlogBlock from "../../components/blog-block/BlogBlock.jsx";
 import data from "../../constants/data.json";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Overview() {
+
+    const [blogs, setBlogs] = useState([]);
+    const [error, toggleError] = useState(false);
+
+    useEffect(() => {
+        fetchBlogs()
+        console.log("Bam!");
+    }, []);
+
+    async function fetchBlogs() {
+        toggleError(false);
+
+        try {
+            const response = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts', {
+                headers: {
+                    'novi-education-project-id': 'd83f1d14-c828-41b5-940b-c58d139b7820'
+                },
+            });
+            setBlogs(response.data);
+            console.log(response.data);
+        } catch(e) {
+            console.error(e);
+            toggleError(true);
+        }
+    }
+
     return (
         <section className="overview-section outer-content-container">
             <div className="inner-content-container">
-                <h1>Bekijk alle {data.length} posts op het platform</h1>
+                <h1>Bekijk alle {blogs.length} posts op het platform</h1>
                 <ul className="post-list">
-                    {data.map((blog) => {
+                    {blogs.map((blog) => {
                         return (
                             <li className="post-item" key={blog.id}>
                                 <BlogBlock
